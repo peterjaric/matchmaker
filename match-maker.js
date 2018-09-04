d025d6f5bce90dab0aa05bf9a76d2b36287949ee
Peter Jaric <register@jaric.org>
2018-09-04 10:36:21 +0200

    Clean up code and add some comments.

--- match-maker.js
+++ match-maker.js
@@ -1,8 +1,9 @@
+// Shuffle an array in place
 // From https://stackoverflow.com/a/12646864/185596
 function shuffleArray(array) {
     for (let i = array.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
-        [array[i], array[j]] = [array[j], array[i]]; // eslint-disable-line no-param-reassign
+        [array[i], array[j]] = [array[j], array[i]];
     }
 }
 
@@ -10,38 +11,38 @@ let nameList = document.querySelector('#names');
 let generate = document.querySelector('#generate');
 let countInput = document.querySelector('#count');
 
+// Disable/enable generate button when the count value changes
 countInput.onkeyup = e => {
 	generate.disabled = countInput.validity.patternMismatch;
 }
 
 generate.onclick = e => {
 	if (countInput.validity.patternMismatch) {
+		// The generate button was enabled although the count value vas invalid,
+		// let's fix that and cancel this operation.
+		generate.disabled = countInput.validity.patternMismatch;
 		return;
 	}
 
 	let results = document.querySelector('#results');
 	let count = countInput.value;
-	var i, names =  Array.from(document.querySelectorAll('#names div')).map(div => div.textContent.trim());
-/*
-	while (names.length % count != 0) {
-		names.push("Empty slot");
-	}
-*/
+	let tables = [];
+	let names =  Array.from(document.querySelectorAll('#names div')).map(div => div.textContent.trim());
 	shuffleArray(names);
 
-	let tables = [];
 	let tableCount = Math.ceil(names.length / count);
-	console.log(tableCount);
-	
-	for (i = 0; i < names.length; i++) {
+
+	// Distribute players as evenly as possible over the available tables
+	for (let i = 0; i < names.length; i++) {
 		if (typeof tables[i % tableCount] == 'undefined') {
 			tables[i % tableCount] = [];
 		}
 		tables[i % tableCount].push(names[i]);
 	}
 
-//	let chunks = chunkArray(names, count);
+	// Create list items for the resulting tables
 	results.innerHTML = tables.map(arr => "<li>" + arr.join("<br>") + "</li>").join(" ");
-	
+
+	// Show list of tables
 	document.querySelector('#results').style.display = 'block';
 };
